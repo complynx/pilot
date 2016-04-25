2f289879159c81d0938ebfb0fab672b700490b99
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-04-25 17:15:19 +0200

    Upd

--- pilot.py
+++ pilot.py
@@ -5,20 +5,30 @@ import logging
 import logging.config
 import argparse
 
+class Pilot:
+    """ Main class """
+
+    def __init__(self):
+        self.argParser = argparse.ArgumentParser(description="This is simplepilot. It will start your task... maybe... in"
+                                                       " some distant future... on a specific environment... with "
+                                                       "the help of some magic...")
+        self.argParser.add_argument("--logconf", type=logging.config.fileConfig, default="loggers.ini",
+                       help="specify logger parameters file", metavar="path/to/loggers.ini")
+
+        self.logger = logging.getLogger("pilot")
+
+    def run(self, argv):
+        self.argParser.parse_args(sys.argv[1:])
+
+        self.logger = logging.getLogger("pilot")
+        self.logger.info("test")
+
 # main
 if __name__ == "__main__":
+    pilot = Pilot()
 
-    argparser = argparse.ArgumentParser(description="This is simplepilot. It will start your task... maybe... in"
-                                                           " some distant future... on a specific environment... with "
-                                                           "the help of some magic...")
-    argparser.add_argument("--logconf", type=logging.config.fileConfig, default="loggers.ini",
-                           help="specify logger parameters file")
+    pilot.run(sys.argv)
 
-    argparser.parse_args(sys.argv[1:])
-
-    # logging.config.fileConfig("loggers.ini")
-    logger = logging.getLogger("pilot")
-    logger.info("test")
 
 
 
