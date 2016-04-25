32fbe76638775b00c401e7ab0d9b010b5a968eb9
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-04-25 16:54:16 +0200

    Logger sets through argparse

--- pilot.py
+++ pilot.py
@@ -3,12 +3,20 @@
 import sys
 import logging
 import logging.config
-# import argparse
+import argparse
 
 # main
 if __name__ == "__main__":
-    logging.config.fileConfig("loggers.ini")
-    log = logging.getLogger("pilot")
-    log.info("test")
+
+    argparser = argparse.ArgumentParser(description="This is simplepilot. It will start your task... maybe... in"
+                                                           " some distant future... on a specific environment... with "
+                                                           "the help of some magic...")
+    argparser.add_argument("--logconf", type=logging.config.fileConfig, default="loggers.ini",
+                           help="specify logger parameters file")
+
+    # logging.config.fileConfig("loggers.ini")
+    logger = logging.getLogger("pilot")
+    logger.info("test")
+
 
 
