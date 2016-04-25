447966cd311bb7e93963e7c4e64e887d71fe286c
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-04-25 17:38:30 +0200

    Upd

--- pilot.py
+++ pilot.py
@@ -5,13 +5,14 @@ import logging
 import logging.config
 import argparse
 
+
 class Pilot:
     """ Main class """
 
     def __init__(self):
-        self.argParser = argparse.ArgumentParser(description="This is simplepilot. It will start your task... maybe... in"
-                                                       " some distant future... on a specific environment... with "
-                                                       "the help of some magic...")
+        self.argParser = argparse.ArgumentParser(description="This is simplepilot. It will start your task... maybe..."
+                                                             " in some distant future... on a specific environment..."
+                                                             " with the help of some magic...")
         self.argParser.add_argument("--logconf", type=logging.config.fileConfig, default="loggers.ini",
                        help="specify logger parameters file", metavar="path/to/loggers.ini")
 
@@ -22,6 +23,11 @@ class Pilot:
 
         self.logger = logging.getLogger("pilot")
         self.logger.info("test")
+
+    def get_queue(self):
+        # curl --connect-timeout 20 --max-time 120 --cacert /tmp/x509up_u500 -sS \"http://pandaserver.cern.ch:25085/cache/schedconfig/ANALY_RRC-KI-HPC.all.json\" > /home/apf/dan_minipilot/queuedata.json
+
+
 
 # main
 if __name__ == "__main__":
