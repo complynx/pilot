cb681e2fb9a6210cb687cc31fbe4f722b1efc2da
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-04-25 16:13:38 +0200

    Logging changed for more info

--- movers/sitemovers.py
+++
@@ -1,9 +0,0 @@
-"""
-  This file contains the list of ENABLED site movers
-"""
-
-from .xrdcp_sitemover import xrdcpSiteMover
-from .dcache_sitemover import dcacheSiteMover
-from .lcgcp_sitemover import lcgcpSiteMover
-
-from .rucio_sitemover import rucioSiteMover
