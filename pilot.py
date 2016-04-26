111b847c379ab95c1073f5e869a16164659b53b8
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-04-26 11:23:24 +0200

    Upd

--- pilot.py
+++ pilot.py
@@ -4,6 +4,8 @@ import sys
 import logging
 import logging.config
 import argparse
+import pycurl
+from StringIO import StringIO
 
 
 class Pilot:
@@ -15,6 +17,8 @@ class Pilot:
                                                              " with the help of some magic...")
         self.argParser.add_argument("--logconf", type=logging.config.fileConfig, default="loggers.ini",
                        help="specify logger parameters file", metavar="path/to/loggers.ini")
+        self.argParser.add_argument("--cacert", type=logging.config.fileConfig, default="",
+                       help="specify CA certificate for your transactions to server", metavar="path/to/x509_uXXX")
 
         self.logger = logging.getLogger("pilot")
 
@@ -24,9 +28,16 @@ class Pilot:
         self.logger = logging.getLogger("pilot")
         self.logger.info("test")
 
+    def create_curl(self, buf = 0):
+        c = pycurl.Curl()
+        if buf == 0:
+            buf = StringIO()
+        c.setopt(c.WRITEDATA, buf)
+        c.setopt(c.CAPATH, )
+
     def get_queue(self):
         # curl --connect-timeout 20 --max-time 120 --cacert /tmp/x509up_u500 -sS \"http://pandaserver.cern.ch:25085/cache/schedconfig/ANALY_RRC-KI-HPC.all.json\" > /home/apf/dan_minipilot/queuedata.json
-
+        self.logger.info("test")
 
 
 # main
