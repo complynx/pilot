b33366d661d80e06efae10fb108240fb89aa80fb
Daniel Driezshuk <d.drizhuk@gmail.com>
2016-07-14 16:31:08 +0200

    Deleted old code. It is in the older commits.

--- job.py
+++ job.py
@@ -139,106 +139,6 @@ class Job(Utility):
                 return
             object.__setattr__(self, key, value)
 
-    # def extract_queuedata_updates(self, job_parameters):
-    #     """
-    #     Extracts the queuedata-overwrite key=value pairs from the job parameters.
-    #     Port from previous version, because this thing is not posix-compliant.
-    #     Also it works awfully unpredictable, so I don't lke the whole of it.
-    #     Ported _only_ for back-compatibility of job descriptions.
-    #     Must be reworked.
-    #     """
-    #     # The dictionary will be used to overwrite existing queuedata values
-    #     # --overwriteQueuedata={key1=value1,key2=value2}
-    #
-    #     queuedata_update_dict = {}
-    #
-    #     # define regexp pattern for the full overwrite command
-    #     pattern = re.compile(r' \-\-overwriteQueuedata=\{.+}')
-    #     full_update_string = re.findall(pattern, job_parameters)
-    #
-    #     if full_update_string and full_update_string[0] != "":
-    #         # tolog("Extracted the full command from the job parameters: %s" % (full_update_string[0]))
-    #         # e.g. full_update_string[0] = '--overwriteQueuedata={key1=value1 key2=value2}'
-    #
-    #         # remove the overwriteQueuedata command from the job parameters
-    #         job_parameters = job_parameters.replace(full_update_string[0], "")
-    #         self.log.info("Removed the queuedata overwrite command from job parameters: %s" % job_parameters)
-    #
-    #         # define regexp pattern for the full overwrite command
-    #         pattern = re.compile(r'\-\-overwriteQueuedata=\{(.+)\}')
-    #
-    #         # extract the key value pairs string from the already extracted full command
-    #         pairs = re.findall(pattern, full_update_string[0])
-    #         # e.g. pairs[0] = 'key1=value1,key2=value2'
-    #
-    #         if pairs[0] != "":
-    #             # tolog("Extracted the key value pairs from the full command: %s" % (pairs[0]))
-    #
-    #             # remove any extra spaces if present
-    #             pairs[0] = pairs[0].replace(" ", "")
-    #
-    #             comma_dictionary = {}
-    #             if "\'" in pairs[0] or '\"' in pairs[0]:
-    #                 self.log.info("Detected quotation marks in the job parameters: %s" % (pairs[0]))
-    #                 # e.g. key1=value1,key2=value2,key3='value3,value4'
-    #
-    #                 # handle quoted key-values separately
-    #
-    #                 # replace any simple qoutation marks with double quotation marks to simplify the regexp below
-    #                 pairs[0] = pairs[0].replace("\'", '\"')
-    #                 pairs[0] = pairs[0].replace('\\"', '\"')  # in case double backslashes are present
-    #
-    #                 # extract all values containing commas
-    #                 comma_list = re.findall('"([^"]*)"', pairs[0])
-    #
-    #                 # create a dictionary with key-values using format "key_%d" = value, where %d is the id of the found
-    #                 #  value
-    #                 # e.g. { key_1: valueX,valueY,valueZ, key_2: valueA,valueB }
-    #                 # replace the original comma-containing value with "key_%d", and replace it later
-    #                 comma_dictionary = {}
-    #                 counter = 0
-    #                 for commaValue in comma_list:
-    #                     counter += 1
-    #                     key = 'key_%d' % counter
-    #                     comma_dictionary[key] = commaValue
-    #                     pairs[0] = pairs[0].replace('\"' + commaValue + '\"', key)
-    #
-    #                 self.log.info("pairs=%s" % (pairs[0]))
-    #                 self.log.info("comma_dictionary=%s" % str(comma_dictionary))
-    #
-    #             # define the regexp pattern for the actual key=value pairs
-    #             # full backslash escape, see (adjusted for python):
-    #             # http://stackoverflow.com/questions/168171/regular-expression-for-parsing-name-value-pairs
-    #             pattern = re.compile(r'((?:\\.|[^=,]+)*)=("(?:\\.|[^"\\]+)*"|(?:\\.|[^,"\\]+)*)')
-    #
-    #             # finally extract the key=value parameters
-    #             kv_ist = re.findall(pattern, pairs[0])
-    #             # e.g. kv_ist = [('key1', 'value1'), ('key2', 'value_2')]
-    #
-    #             # put the extracted pairs in a proper dictionary
-    #             if kv_ist:
-    #                 self.log.info("Extracted the following key value pairs from job parameters: %s" % str(kv_ist))
-    #
-    #                 for key, value in kv_ist:
-    #
-    #                     if key != "":
-    #                         # extract the value from the comma_dictionary if it exists
-    #                         if value in comma_dictionary:
-    #                             value = comma_dictionary[value]
-    #
-    #                         queuedata_update_dict[key] = value
-    #                     else:
-    #                         self.log.warning("Bad key detected in key value tuple: %s" % str((key, value)))
-    #             else:
-    #                 self.log.warning("Failed to extract the key value pair list from: %s" % (pairs[0]))
-    #         else:
-    #             self.log.warning("Failed to extract the key value pairs from: %s" % (pairs[0]))
-    #     else:
-    #         self.log.warning("Failed to extract the full queuedata overwrite command from jobParameters=%s" %
-    #                          job_parameters)
-    #
-    #     return job_parameters, queuedata_update_dict
-
     def prepare_command_params(self):
         """
         Splits command parameters and extracts queuedata modifications if present.
@@ -303,25 +203,6 @@ class Job(Utility):
 
         self.log.debug("Prepared parameters: %s" % " ".join(pipes.quote(x) for x in new_params))
         self.command_parameters = new_params
-
-    # def modify_queuedata(self):
-    #     """
-    #     Finds and parses parameters to be overwritten, then modifies queuedata accordingly.
-    #     """
-    #     params = self.command_parameters
-    #     modifier = {}
-    #     others = ""
-    #     if isinstance(params, basestring) and '--overwriteQueuedata' in params:
-    #         others, modifier = self.extract_queuedata_updates(str(params))
-    #
-    #         self.log.debug("overwrite: %s" % (modifier))
-    #         self.log.debug("params: %s" % (others))
-    #
-    #         for key in modifier:
-    #             self.pilot.queuedata[key] = modifier[key]
-    #
-    #         self.log.info("queuedata modified.")
-    #         self.log.debug("queuedata: " + json.dumps(self.pilot.queuedata, indent=4))
 
     def init_logging(self):
         """
